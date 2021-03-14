de9e96558ea30e3b6ce27b85ef30100983deac62
Hussein Faara <faarahussein@gmail.com>
2021-03-14 01:58:31 -0800

    passed sixth test case

--- sql/06.sql
+++ sql/06.sql
@@ -6,3 +6,14 @@
  * That is, list all actors that have appeared in a film with 'RUSSEL BACALL'.
  * Do not list 'RUSSEL BACALL', since he has a Bacall Number of 0.
  */
+select distinct concat(first_name, ' ', last_name) as "Actor Name" 
+from actor 
+join film_actor using (actor_id) 
+inner join (
+    select film_id 
+    from actor 
+    inner join film_actor using (actor_id) 
+    where first_name='RUSSELL' and last_name='BACALL'
+) as a using (film_id) 
+where concat(first_name, ' ', last_name)!='RUSSELL BACALL' 
+order by "Actor Name";
