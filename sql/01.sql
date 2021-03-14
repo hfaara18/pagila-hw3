5e00ba8e5157bdf3d47e244018ca9ad73217375c
Hussein Faara <faarahussein@gmail.com>
2021-03-14 00:12:57 -0800

    passed first test case

--- sql/01.sql
+++ sql/01.sql
@@ -1,3 +1,13 @@
 /*
  * Select the title of all 'G' rated movies that have the 'Trailers' special feature.
  */
+select title 
+from film 
+where rating='G' 
+intersect 
+(
+    select title 
+    from film 
+    where 'Trailers'=any(special_features)
+) 
+order by title desc;
