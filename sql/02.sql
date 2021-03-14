5cee3e2e58a0e5bb4ac4fa6edacba1fbd0977521
Hussein Faara <faarahussein@gmail.com>
2021-03-14 00:45:56 -0800

    passed second test case

--- sql/02.sql
+++ sql/02.sql
@@ -1,3 +1,10 @@
 /*
  * Count the number of movies that contain each type of special feature.
  */
+select special_features, count(*) 
+from (
+    select unnest(special_features) as special_features
+    from film
+) f 
+group by special_features
+order by special_features;
