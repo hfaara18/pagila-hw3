79708f23592a68234ca8272af3aa1363873506ba
Hussein Faara <faarahussein@gmail.com>
2021-03-14 01:17:38 -0800

    passed fourth test case

--- sql/04.sql
+++ sql/04.sql
@@ -1,3 +1,9 @@
 /*
  * List the name of all actors who have appeared in a movie that has the 'Behind the Scenes' special_feature
  */
+select distinct first_name || ' ' || last_name as "Actor Name" 
+from film 
+join film_actor using (film_id) 
+join actor using (actor_id) 
+where 'Behind the Scenes'=any(special_features)
+order by "Actor Name";
